a9bdbce8d1ff0b4c9d178e28d75d5fb4a2aa11d0
Ferry Ras <fras@student.codam.nl>
2023-02-14 19:41:18 +0100

    corrected bitsize in write

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/23 00:40:47 by fras          #+#    #+#                 */
-/*   Updated: 2023/02/14 13:53:02 by fras          ########   odam.nl         */
+/*   Updated: 2023/02/14 18:01:01 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 void	error(void)
 {
-	write(2, _RED "ft_printf error: ", 25);
+	write(2, _RED "ft_printf error: ", 24);
 }
 
 int	error_invalid_conversion(char conversion)
 {
 	error();
-	write(2, _WHITE "invalid conversion specifier.. '", 40);
+	write(2, _WHITE "invalid conversion specifier.. '", 39);
 	write(2, &conversion, 1);
-	write(2, "'" _RESET, 6);
+	write(2, "'" _RESET, 5);
 	return (-1);
 }
