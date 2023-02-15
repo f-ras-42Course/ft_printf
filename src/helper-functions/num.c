c08281c40751441f78b9b0d28b578caa1b1dda33
Ferry Ras <fras@student.codam.nl>
2023-02-15 12:44:55 +0100

    putnbr update - better structurized

--- src/helper-functions/num.c
+++ src/helper-functions/num.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/14 16:03:23 by fras          #+#    #+#                 */
-/*   Updated: 2023/02/14 19:46:36 by fras          ########   odam.nl         */
+/*   Updated: 2023/02/15 12:38:34 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,28 @@ int	pfhelper_putnbr(va_list ap)
 
 	num = va_arg(ap, int);
 	len = 0;
-	if (num == -2147483648)
-		return (write(1, "-2147483648", 11));
-	if (num == 0)
-		return (write(1, "0", 1));
-	if (num < 0)
-	{
-		len += write(1, "-", 1);
-		num *= -1;
-	}
 	len += helper_numprint(num, &len);
 	return (len);
 }
 
 int	helper_numprint(int num, int *len)
 {
-	if (num)
+	if (num >= 0 && num <= 9)
+		return (helper_printer(num + '0'));
+	if (num >= 10)
 	{
 		*len += helper_numprint(num / 10, len);
 		return (helper_printer(num % 10 + '0'));
+	}
+	if (num < 0)
+	{
+		if (num == -2147483648)
+			return (write(1, "-2147483648", 11));
+		else
+		{
+			*len += helper_printer('-');
+			return (helper_numprint(num * -1, len));
+		}
 	}
 	return (0);
 }
