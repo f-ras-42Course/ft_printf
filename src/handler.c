b53a62a3eb73c105799a6cbd8f3cf6ad1a1b1463
Ferry Ras <fras@student.codam.nl>
2023-02-15 14:42:57 +0100

    removed static

--- src/handler.c
+++ src/handler.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/16 20:09:43 by fras          #+#    #+#                 */
-/*   Updated: 2023/02/14 18:36:37 by fras          ########   odam.nl         */
+/*   Updated: 2023/02/15 14:42:05 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int	conversions(char conversion)
 
 int	print_conversion(va_list ap, const char index)
 {
-	static t_Jump_index	convert[] = {
+	t_Jump_index	convert[] = {
 	['c'] = &helper_putchar,
 	['s'] = &helper_putstr,
 	['i'] = &pfhelper_putnbr,
